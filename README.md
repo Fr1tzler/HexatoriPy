1753612333741f915ab0bab8fcea88845e23fa22
fr1tzler <fritzlerv@yandex.ru>
2020-12-02 17:02:27 +0500

    fix level size slider

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # HexatoriPy
 Hitori game hex variation
+
+RULES:
+
+You need to color the hex map, using theese two rules:
+1) There should be no hexagons with equal value in a row/column, so you can delete hexagon coloring it black (dark green)
+2) Hexagons, colored black (dark green) shouldn't be neighbours
--- pyQt/main.py
+++ pyQt/main.py
@@ -6,9 +6,10 @@ import design
 sys.path.append(os.path.join(sys.path[0], '../Model'))
 from hexatori_generator import get_map
 from hexatori_solver import HexatoriSolver
+from hexatori import Hexatori
 
 
-class HexatoriApp(QtWidgets.QMainWindow, design.Ui_HexatoriPy):
+class HexatoriApp(QtWidgets.QMainWindow, design.Ui_HexatoriPy, Hexatori):
     black_color = "#00665E"
     background_color = "#33CEC3"
     unchecked_color = "#DF8330"
@@ -17,23 +18,28 @@ class HexatoriApp(QtWidgets.QMainWindow, design.Ui_HexatoriPy):
     def __init__(self, map_size):
         super().__init__()
         self.setupUi(self)
-        self.setStyleSheet("background-color:" + self.background_color + ";")
 
-        self.ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
         self.map_size = map_size
         self.array_size = map_size * 2 - 1
 
+        self.init_styles()
+        self.groupBox.setStyleSheet(self.checked_style)        
         self.init_buttons()
+        self.init_size_slider()
         self.buttons = []
         self.generate_map()
         self.mode = 0
         self.show()
 
+    def init_styles(self):
+        self.black_style = "background-color:" + self.black_color + "; color: white;"
+        self.checked_style = "background-color:" + self.checked_color + "; color: black;"
+        self.unchecked_style = "background-color:" + self.unchecked_color + "; color: black;"
+        self.setStyleSheet("background-color:" + self.background_color + ";")
+
     def init_buttons(self):
         self.newGameButton.move(700, 100)
         self.init_button(self.newGameButton, self.new_game_click, "new game")
-        self.hintButton.move(635, 195)
-        self.init_button(self.hintButton, self.hintClick, "hint")
         self.solveButton.move(700, 290)
         self.init_button(self.solveButton, self.solve_click, "solve")
         self.blackButton.move(-30, 450)
@@ -46,11 +52,40 @@ class HexatoriApp(QtWidgets.QMainWindow, design.Ui_HexatoriPy):
     def init_button(self, button, onclick, text):
         button.setText(text)
         self.hexagonize_button(button)
-        button.setStyleSheet("background-color:" + self.black_color + ";")
+        button.setStyleSheet(self.black_style)
         button.clicked.connect(onclick)
 
     def new_game_click(self):
+        #self.slider.setVisible(True) ###
         self.generate_map()
+
+    def init_size_slider(self):
+        self.groupBox.resize(120, 120)
+        self.groupBox.move(635, 195)
+        self.hexagonize_button(self.groupBox)
+        self.groupBox.setStyleSheet(self.black_style)
+        
+        temp_stylesheet = "color: white; background-color:" + self.black_color + ";"
+
+        self.size_label.setText("5")
+        self.size_label.setFont(QtGui.QFont("Ubuntu BOLD", 20))
+        self.size_label.setStyleSheet(temp_stylesheet)
+        self.size_label.move(685, 220)
+        self.size_label.resize(60, 30)
+        self.size_label.show()
+
+        self.horizontalSlider.setMinimum(3)
+        self.horizontalSlider.setMaximum(10)
+        self.horizontalSlider.setSingleStep(1)
+        self.horizontalSlider.valueChanged.connect(self.slider_change)
+        self.horizontalSlider.resize(110, 20)
+        self.horizontalSlider.setStyleSheet(temp_stylesheet)
+        self.horizontalSlider.move(640, 260)
+
+    def slider_change(self):
+        value = self.horizontalSlider.value()
+        self.size_label.setText(str(value))
+        self.map_size = value
 
     def hintClick(self):
         pass
@@ -97,26 +132,22 @@ class HexatoriApp(QtWidgets.QMainWindow, design.Ui_HexatoriPy):
                     continue
                 button = QtWidgets.QPushButton(self.hex_map[x][y], self)
                 button.resize(button_width, button_height)
-                button.move(*self.get_position(x, y, button_width,
-                                               button_height, offset))
+                button.move(*self.get_position(x, y, button_width, button_height, offset))
                 self.hexagonize_button(button)
                 self.buttons.append(button)
-                button.setStyleSheet(
-                    "background-color:" + self.unchecked_color + "; color: black;")
-                button.setFont(QtGui.QFont(
-                    "Ubuntu BOLD", 200 // self.array_size))
+                button.setStyleSheet(self.unchecked_style)
+                button.setFont(QtGui.QFont("Ubuntu BOLD", 200 // self.array_size))
                 button.show()
         for y in range(self.array_size):
             for x in range(self.array_size):
                 button_id = y * self.array_size + x
                 if self.buttons[button_id] is None:
                     continue
-                self.buttons[button_id].clicked.connect(
-                    lambda temp, arg=button_id: self.tile_click(arg))
+                self.buttons[button_id].clicked.connect(lambda temp, arg=button_id: self.tile_click(arg))
 
     def get_position(self, x, y, button_width, button_height, offset):
-        bw = button_width + 2
-        bh = button_height * 3 // 4 + 4
+        bw = button_width + 1
+        bh = button_height * 3 // 4 + 3
         return 5 + bw * x + offset * bw // 2, 40 + bh * y
 
     def hexagonize_button(self, button):
@@ -130,7 +161,7 @@ class HexatoriApp(QtWidgets.QMainWindow, design.Ui_HexatoriPy):
         p6 = QtCore.QPoint(0, y // 4)
         points = [p1, p2, p3, p4, p5, p6]
         button.setMask(QtGui.QRegion(QtGui.QPolygon(points)))
-        button.setStyleSheet("background-color: white; border: none")
+        button.setStyleSheet("border: none")
 
     def tile_click(self, button_id):
         x, y = button_id % self.array_size, button_id // self.array_size
@@ -147,15 +178,15 @@ class HexatoriApp(QtWidgets.QMainWindow, design.Ui_HexatoriPy):
     def set_tile_black(self, x, y):
         status = self.solver.set_point_black([x, y])
         if status:
-            self.buttons[y * self.array_size + x].setStyleSheet(
-                "background-color:" + self.black_color + "; color: white;")
+            self.buttons[y * self.array_size + x].setStyleSheet(self.black_style)
+        for neighbour in self.solver.get_point_neighbours([x, y]):
+            self.buttons[neighbour[1] * self.array_size + neighbour[0]].setStyleSheet(self.checked_style)
         return status
 
     def set_tile_white(self, x, y):
         status = self.solver.set_point_white([x, y])
         if status:
-            self.buttons[y * self.array_size + x].setStyleSheet(
-                "background-color:" + self.checked_color + "; color: black;")
+            self.buttons[y * self.array_size + x].setStyleSheet(self.checked_style)
         return status
 
     def alert(self, message):
@@ -169,7 +200,7 @@ class HexatoriApp(QtWidgets.QMainWindow, design.Ui_HexatoriPy):
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    window = HexatoriApp(20)
+    HexatoriApp(4)
     app.exec_()
 
 
@@ -178,6 +209,5 @@ if __name__ == "__main__":
 
 """
 TODO
-finish solver
-hintClick()
+alert()
 """
